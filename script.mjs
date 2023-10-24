f232dfed237b15208986e30f18db8dbee11977f0
Brian <bperin42@gmail.com>
2023-10-23 23:15:56 -0700

    cleanup

--- script.mjs
+++ script.mjs
@@ -1,21 +1,28 @@
 import path from 'path';
 import { throttledTranscription } from './transcribe.mjs';
 
-if (process.argv.length !== 3) {
-    console.log('Usage: node script.js <file_path>');
-    process.exit(1); // Exit the script with an error code
-}
+async function main() {
+    if (process.argv.length !== 3) {
+        console.error('Usage: node script.js <file_path>');
+        process.exit(1); // Exit the script with an error code
+    }
 
-const filePath = process.argv[2];
+    const filePath = process.argv[2];
 
-if (path.extname(filePath).toLowerCase() !== '.wav') {
-    console.log('Please provide a WAV file.');
-    process.exit(1); // Exit the script with an error code
-}
-throttledTranscription.transcribeAudio(filePath)
-    .then(result => {
+    if (path.extname(filePath).toLowerCase() !== '.wav') {
+        console.error('Please provide a WAV file.');
+        process.exit(1); // Exit the script with an error code
+    }
+
+    try {
+        const result = await throttledTranscription.transcribeAudio(filePath);
         console.log(JSON.stringify(result, null, 2));
-    })
-    .catch(error => {
-        console.log(JSON.stringify(error, null, 2));
-    });
+    } catch (error) {
+        console.error(JSON.stringify(error, null, 2));
+    }
+}
+
+main().catch((error) => {
+    console.error('An unexpected error occurred:', error);
+    process.exit(1);
+});
